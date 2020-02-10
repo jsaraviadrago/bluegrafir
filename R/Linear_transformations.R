5fa293d0a40dd29bb39186fbcec2777ec92c7715
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2020-02-10 10:44:49 -0500

    Updating global variables function, qnorm

--- R/Linear_transformations.R
+++ R/Linear_transformations.R
@@ -2,7 +2,7 @@
 #'
 #' This function is used to standardize your results and transform them with a set mean and standard deviation.
 #'
-#' @name bluebase
+#' @name blue_ztransform
 #' @param x vector that you want to change scale.
 #' @param meanvar parameter for the mean, the default is 500. This is used for the linear transformation and setting a mean different from 0
 #' @param sdvar parameter for the standard deviation, the default is 100. This is used for the linear transformation and setting a standard deviation different from 1
@@ -10,14 +10,14 @@
 #' @author Juan Carlos Saravia
 #' @examples \donttest{blue(x,y,z)}
 #' @export
-bluebase <- function(x,meanvar = 500,sdvar = 100) {
+blue_ztransform <- function(x,meanvar = 500,sdvar = 100) {
   standard <- scale(x, center = T, scale = T)
   conversion <- (standard*sdvar)+meanvar
 }
 
 #' This function is used to compare results between groups or years with parameters that you establish.
 #'
-#' @name bluecomp
+#' @name blue_comparison
 #' @param lmes vector with the most recent measurement or the group you want to compare against the original parameters
 #' @param parfmean mean with the baseline parameter to compare against.
 #' @param parfsd sd with the baseline parameter to compare against.
@@ -28,7 +28,7 @@ bluebase <- function(x,meanvar = 500,sdvar = 100) {
 #' @examples \donttest{blue2(x,y,z,a)}
 #' @export
 
-bluecomp <- function(lmes, parfmean, parfsd,
+blue_comparison <- function(lmes, parfmean, parfsd,
                   meanvar = 500, sdvar = 100) {
   equate <- (lmes-parfmean)/parfsd
   conversion <- (equate*sdvar)+meanvar
@@ -38,23 +38,23 @@ bluecomp <- function(lmes, parfmean, parfsd,
 #'
 #'It calculates the cumulative frequency of a continous variable, transforms this variable into percentiles by using a constant of .50 in order to calculate values over the 50% and under it based on the frequency of values.
 #' After that, it standardize scores and then transforms values with a mean of 500 and standard deviation of 100 (default).
-#' @name bluebare
+#' @name blue_zscaling
 #' @param x vector of continous values
 #' @param sdev standard deviation which is to be assigned for the transformation, default value is 100.
 #' @param means mean which is to be assigned for the linear transformation. Default value is 500.
-#' @param threshold logical parameter were TRUE assigns three thresholds. It is based on a normal distribution were values lower than -1 standard deviation from the mean are low, values higher than 1 standard deviation are high and values in between are medium (default).
+#' @param type_scale logical parameter were TRUE assigns three thresholds. It is based on a normal distribution were values lower than -1 standard deviation from the mean are low, values higher than 1 standard deviation are high and values in between are medium (default).
 #' if logical parameter is FALSE, 5 thresholds are assigned where values bigger than 2 standard deviations are very high, higher than 1 standard deviation are high, higher than -1 are medium, higher than -2 are low and lower than -2 standard deviations are very low.
 #' @return The output is a tibble with raw scores, zscores and thresholds
 #' @importFrom dplyr mutate
 #' @importFrom dplyr case_when
 #' @importFrom dplyr select
+#' @importFrom rlang .data
 #' @author Juan Carlos Saravia
-#' @examples \donttest{bluebare(x,sdev,means,threshold)}
+#' @examples \donttest{blue_zscaling(x,sdev,means,threshold)}
 #' @export
-globalVariables (c("x", "Zscore", "cortes",
-                   "Z", "pvalue", "std.all",
-                   "Puntajes_Brutos", "Threshold"))
-bluebare <- function(x, sdev = 100, means = 500, threshold = TRUE){
+
+blue_zscaling <- function(x, sdev = 100, means = 500,
+                          type_scale = "CumulativeZ"){
   order.freq <- data.frame(table(x))
   order.freq$CumFreq <- cumsum(order.freq$Freq)
   val1 <- data.frame((0.5*order.freq$Freq)/(sum(order.freq$Freq))*100)
@@ -71,38 +71,27 @@ bluebare <- function(x, sdev = 100, means = 500, threshold = TRUE){
     select(-freqrp, -RPcalc)
   order.freq
   order.freq$RPdiv100 <- (order.freq$RP)/100
-
-  order.freq$Zscore <- scale(order.freq$RPdiv100,
-                             center = T,
-                             scale = T)
-  order.freq$Tscore <- ((order.freq$Zscore)*sdev)+means
-
-  if (threshold == TRUE) {
-    order.freq <- order.freq %>% mutate(
-      cortes = case_when(
-        Zscore > 1 ~ "Alto",
-        Zscore > -1 ~ "Medio",
-        TRUE ~ "Bajo"))
+  if (type_scale == "CumulativeZ") {
+    order.freq$Zscore <- stats::qnorm(order.freq$RPdiv100)
+  } else if(type_scale == "Zscore"){
+      order.freq$Zscore <- scale(order.freq$RPdiv100,
+                               center = T,
+                               scale = T)
   } else {
-    order.freq <- order.freq %>% mutate(
-      cortes = case_when(
-        Zscore > 2 ~ "Muy alto",
-        Zscore > 1 ~ "Alto",
-        Zscore > -1 ~ "Medio",
-        Zscore > -2 ~ "Bajo",
-        TRUE ~ "Muy bajo"))
+    order.freq$Zscore <- stats::qnorm(order.freq$RPdiv100)
   }
+  order.freq$Tscore <- ((order.freq$Zscore)*sdev)+means
   baremos <- order.freq %>%
-    select(Puntajes_Brutos = x,
-           Z = Zscore,
-           Threshold = cortes)
+    select(Puntajes_Brutos = .data$x,
+           Z = .data$Zscore,
+           Threshold = .data$cortes)
   baremos <- dplyr::as.tbl(baremos)
   summarytab <- baremos %>%
-    dplyr::group_by(Threshold) %>%
-    dplyr::summarise(MinPB = dplyr::first(Puntajes_Brutos),
-              MaxPB = dplyr::last(Puntajes_Brutos),
-              MinZ = min(Z, na.rm = T),
-              MaxZ = max(Z, na.rm = T))
+    dplyr::group_by(.data$Threshold) %>%
+    dplyr::summarise(MinPB = dplyr::first(.data$Puntajes_Brutos),
+              MaxPB = dplyr::last(.data$Puntajes_Brutos),
+              MinZ = min(.data$Z, na.rm = T),
+              MaxZ = max(.data$Z, na.rm = T))
   list(baremos,summarytab)
 }
 
@@ -111,7 +100,7 @@ bluebare <- function(x, sdev = 100, means = 500, threshold = TRUE){
 #' It calculates mean-mean parameters in order to do a linear transformation of a scale to make it comparable with another one.
 #' The function does the mean-mean transformation for test in a IRT framework, a good reference is:
 #' Kolen, M & Brennan, R. (2014) Test equating, scaling and linking. Methods and practice. Third edition. NY: Springer.
-#' @name bluequate
+#' @name blue_equate
 #' @param x vector of continous variable that you want to equate
 #' @param y vector of continous variable that you want to equate to. This variable will have the same metric as parameter x.
 #' @return The output is a list of tibbles with the calculated parameters, the items and the difficulty of items of one group and the other. Finally a the plot with control bands to check item displacement.
--- R/performance_tables.R
+++ R/performance_tables.R
@@ -1,7 +1,7 @@
 #'Table of overall fit measures for a CFA and SEM models
 #'
 #'This function needs a lavaan object with a model fit to work. It creates a table of all fit indices for a SEM model.
-#' @name grafit
+#' @name grafi_fit
 #' @param x lavaan object: The name of the model fit that was calculated from the specified CFA or SEM model in the lavaan package.
 #' @param mi.nrows specify the amount of rows to show in the modification indexes table. The default is 5 rows.
 #' @return The output is a list of tibbles that gives you overall fit indices, parameter estimates and modification indices for CFA or SEM models.
@@ -14,15 +14,15 @@
 #' @importFrom dplyr filter
 #' @importFrom dplyr recode
 #' @importFrom lavaan parameterEstimates
+#' @importFrom rlang .data
 #' @author Juan Carlos Saravia
 #' @examples \donttest{grafi2(fit)}
 #' @export
 #'
 #'
 #'
-globalVariables (c("indices", "rhs", "se",
-                   "z", "pvalue", "std.all"))
-grafit <- function(x, mi.nrows = 5) {
+
+grafi_fit <- function(x, mi.nrows = 5) {
   tabla <- data.frame(lavaan::fitMeasures(x))
   colnames(tabla)[1] <- "Fit Measures"
   names <- c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr",
@@ -31,7 +31,7 @@ grafit <- function(x, mi.nrows = 5) {
              "tli.scaled", "rmsea.scaled")
   tabla$indices <- rownames(tabla)
   tabla <- tabla %>%
-    filter(indices %in% names)
+    filter(.data$indices %in% names)
   estimador <- nrow(tabla)
   names2 <- c("Chi2", "DF", "P-VALUE", "CFI", "TLI", "RMSEA", "SRMR")
   if (estimador == 7) {
@@ -53,7 +53,7 @@ grafit <- function(x, mi.nrows = 5) {
                                     rmsea.scaled = "RMSEA",
                                     srmr = "SRMR")
     tabla <- tabla %>%
-      filter(indices %in% names2)
+      filter(.data$indices %in% names2)
   }
   tabla <- tabla[,c(2,1)]
   analysis <- c("~", "~~", ":=")
@@ -61,27 +61,27 @@ grafit <- function(x, mi.nrows = 5) {
     filter(op %in% analysis) %>%
     select('Dep variable'=lhs,
            Relationship=op,
-           'Ind variable'=rhs,
+           'Ind variable'=.data$rhs,
            B=est,
-           SE=se,
-           Z=z,
-           'p-value'=pvalue,
-           Beta=std.all)
+           SE=.data$se,
+           Z=.data$z,
+           'p-value'=.data$pvalue,
+           Beta=.data$std.all)
   tabla2 <- parameterEstimates(x, standardized=TRUE) %>%
     filter(op == "=~") %>%
     select('Latent factor'=lhs,
-           'Variable'=rhs,
+           'Variable'=.data$rhs,
            B=est,
-           SE=se,
-           Z=z,
-           'p-value'=pvalue,
-           Beta=std.all)
+           SE=.data$se,
+           Z=.data$z,
+           'p-value'=.data$pvalue,
+           Beta=.data$std.all)
   mi <- lavaan::inspect(x,"mi")
   mi.order <- mi[order(-mi$mi),]
   tabla3 <- mi.order[1:mi.nrows,] %>%
     select(Variable_1 = lhs,
            relationship = op,
-           Variable_2 = rhs,
+           Variable_2 = .data$rhs,
            MI = mi)
   tabla <- as.tbl(tabla)
   tabla1 <- as.tbl(tabla1)
@@ -96,30 +96,30 @@ grafit <- function(x, mi.nrows = 5) {
 #'
 #' This function needs a psych and GPArotation object from the omegaSem function to work. It creates a table of all fit indices for a CFA and SEM model.
 #'
-#' @name grafirel
+#' @name grafi_reliability
 #' @param x McDonalds Omega calculated with the omegaSem function. The name of the McDonald's Omega specified with the psych and GPArotation packages.
 #' @return The output is a data.frame that gives you fit McDonalds Omega coefficients in a table.
 #' @importFrom dplyr "%>%"
 #' @importFrom dplyr select
+#' @importFrom rlang .data
 #' @author Juan Carlos Saravia
 #' @examples \donttest{grafi2(fit)}
 #' @export
-globalVariables(c("total", "omegarel", "varmax",
-                  "Factor", "Omega"))
-grafirel <- function(x) {
+
+grafi_reliability <- function(x) {
   # Extracting Omega reliability coeficients
   omegacoef <- data.frame(x$omegaSem$omega.group)
   factor.names <- rownames(omegacoef)
   factor.names <- factor.names[!factor.names %in%
                                  "g"]
   omegacoef <- omegacoef %>%
-    select(total)
+    select(.data$total)
   omegacoef$names <- rownames(omegacoef)
   omegacoef <- data.frame(omegacoef[2:nrow(omegacoef[1]),])
 
   omegacoef$names <- gsub("[*]", "", omegacoef$names)
   # Check which reliability stands for which factor.
-  factors <- omegarel$omegaSem$schmid$sl
+  factors <- .data$omegarel$omegaSem$schmid$sl
   factors <- factors[,c(factor.names)]
   factors <- data.frame(factors)
   factors <- abs(factors)
@@ -127,15 +127,15 @@ grafirel <- function(x) {
                                               ties.method="first")]
   factors$names <- rownames(factors)
   factors <- factors %>%
-    select(names, varmax)
+    select(names, .data$varmax)
   factors$varmax <- gsub("[.]", "", factors$varmax)
   factors <- dplyr::left_join(factors, omegacoef,
                        by = c("varmax" = "names"))
   factors <- factors %>%
-    select(Factor = varmax,
-           Omega = total)  %>%
-    dplyr::group_by(Factor) %>%
-    dplyr::summarise(Omega = mean(Omega))
+    select(Factor = .data$varmax,
+           Omega = .data$total)  %>%
+    dplyr::group_by(.data$Factor) %>%
+    dplyr::summarise(Omega = mean(.data$Omega))
   factors
 }
 
@@ -144,26 +144,27 @@ grafirel <- function(x) {
 #'
 #' This function just needs a vector and it is useful if you want to make a tidy table of distributions of items. It works with the map function from the purrr package which enables you to calculate multiple ordered tables of items.
 #'
-#' @name graficat
-#' @param y vector of values that show a table of frequencies, proportions and cumulative frequencies.
+#' @name grafi_distribution
+#' @param x vector of values that show a table of frequencies, proportions and cumulative frequencies.
 #' @return a tibble of 4 columns with categories, frequencies, proportions and cumulative frequencies.
 #' @importFrom dplyr "%>%"
+#' @importFrom rlang .data
 #' @author Juan Carlos Saravia
-#' @examples \donttest{grafi3(x)}
+#' @examples \donttest{grafi_distribution(x)}
 #' @export
 #'
 #'
-globalVariables(c("Freq.x", "Freq.y", "x",
-                  "Frequencies"))
-graficat <- function(x) {
+#globalVariables(c("x"))
+grafi_distribution <- function(x) {
   freq <- data.frame(table(x))
   prop <- data.frame(prop.table(table(x)))
   tabla <- dplyr::left_join(freq,prop, by = "x")
+  names(tabla)[1] <- "Vector1"
   tabla <- tabla %>%
-    dplyr::select(Categories = x,
-           Frequencies = Freq.x,
-           Proportions = Freq.y)
-  tabla <- transform(tabla, CumFreq = cumsum(Frequencies))
+    dplyr::select(Categories = .data$Vector1,
+           Frequencies = .data$Freq.x,
+           Proportions = .data$Freq.y)
+  tabla <- transform(tabla, CumFreq = cumsum(.data$Frequencies))
   tabla <- dplyr::as.tbl(tabla)
   tabla
 }
