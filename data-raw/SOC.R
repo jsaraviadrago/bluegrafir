bfe9396c5ff67b1ed5057e01c1853058e8eef33e
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2020-02-10 16:34:22 -0500

    Putting data into my package

---
+++ data-raw/SOC.R
@@ -0,0 +1,38 @@
+## code to prepare `SOC` dataset goes here
+
+#' @title SOC
+#' @description Dataset of a study of Sense of Coherence (SOC) in a Peruvian sample
+#' @docType data
+#' @usage data(SOC)
+#'
+#' @format A data frame with 155 rows and 26 variables:
+#' \describe{
+#'   \item{\code{Id}}{integer simple id of every person in the dataset}
+#'   \item{\code{Sexo}}{character sex of participants just categorised as men and women}
+#'   \item{\code{Edad}}{integer age of every participant}
+#'   \item{\code{Nivel_educativo}}{character educational level, primary school, secondary school and university}
+#'   \item{\code{SOC_1}}{integer item with likert scale between 1 to 7 that measures SOC }
+#'   \item{\code{SOC_2}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_3}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_4}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_5}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_6}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_7}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_8}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_9}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_10}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_11}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_12}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_13}}{integer item with likert scale between 1 to 7 that measures SOC}
+#'   \item{\code{SOC_1_INV}}{integer item with likert scale between 1 to 7 that measures SOC, inverted item}
+#'   \item{\code{SOC_2_INV}}{integer item with likert scale between 1 to 7 that measures SOC, inverted item}
+#'   \item{\code{SOC_3_INV}}{integer item with likert scale between 1 to 7 that measures SOC, inverted item}
+#'   \item{\code{SOC_7_INV}}{integer item with likert scale between 1 to 7 that measures SOC, inverted item}
+#'   \item{\code{SOC_10_INV}}{integer item with likert scale between 1 to 7 that measures SOC, inverted item}
+#'   \item{\code{TOTAL_SOC}}{integer Total Sense of coherence calculated by adding all items, here inverted ítems have been used in ítems 1,2,3,7,10}
+#'   \item{\code{Comprensibility}}{integer Total Comprehensibility calculated by adding ítems}
+#'   \item{\code{Manejability}}{integer Total Manejeability calculated by adding ítems}
+#'   \item{\code{Meaningfulness}}{integer Meaningfulness}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"SOC"
