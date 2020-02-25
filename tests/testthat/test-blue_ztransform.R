95cdf21bf70869a5292ca07db437220022ee8eed
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2020-02-25 13:59:50 -0500

    Updating test that

--- tests/testthat/test-blue_ztransform.R
+++ tests/testthat/test-blue_ztransform.R
@@ -2,13 +2,17 @@ test_that("Make a simple linear transformation", {
 
   data_example <- data.frame(ID = c(1,2,3,4,5),
                               puntaje = c(1,2,3,4,5))
+
   data_result <- data.frame(ID = c(1,2,3,4,5),
-                            puntaje = c(1,2,3,4,5),
-                            Transf_500 = c(373.5089, 436.7544,
-                                           500.0000, 563.2456,
-                                           626.4911))
-   expect_equivalent(blue_ztransform(data_example$puntaje, data_example),
-                     data.frame(data_result))
+                            puntaje = c(1,2,3,4,5))
+  meanvar <- 500
+  sdvar <- 100
+  z <- (scale(data_example$puntaje,
+                                  center = T, scale = T)*sdvar)+meanvar
+  data_result$Transf_500 <- as.numeric(z)
+  expect_equal(blue_ztransform(data_example$puntaje, data_example),
+                     data_result)
+  expect_error(blue_ztransform(data_example$puntaje))
 })
 
 
