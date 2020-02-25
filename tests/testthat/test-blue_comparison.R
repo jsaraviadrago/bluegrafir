7406f131fa3928accc3d16de4dae9c29ad0c9ae1
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2020-02-25 14:59:52 -0500

    Update tests

--- tests/testthat/test-blue_comparison.R
+++ tests/testthat/test-blue_comparison.R
@@ -1,3 +1,24 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Comparison of two scales", {
+  data_example <- data.frame(ID = c(1,2,3,4,5),
+                             puntaje = c(1,2,3,4,5))
+
+  data_result <- data.frame(ID = c(1,2,3,4,5),
+                            puntaje = c(1,2,3,4,5))
+  parfmean <- 1
+  parfsd <- 0.5
+  meanvar <- 500
+  sdvar <- 100
+
+  data_result$Transf_500 <- (data_example$puntaje-parfmean)/parfsd
+  data_result$Transf_500 <- (data_result$Transf_500*sdvar)+meanvar
+  expect_equal(blue_comparison(data_example$puntaje,
+                               data_example,
+                               parfmean = 1,
+                               parfsd = 0.5),
+               data_result)
+  expect_error(blue_comparison(data_example$puntaje,
+                               data_example))
 })
+
+
+
