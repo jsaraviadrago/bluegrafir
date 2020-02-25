438416f9bd00fd6c5f3f534b5c3bd2ac39ec0bd6
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2020-02-25 10:56:56 -0500

    Updating

--- tests/testthat/test-blue_ztransform.R
+++ tests/testthat/test-blue_ztransform.R
@@ -1,17 +1,14 @@
+context("running linear transformation")
+
 test_that("Make a simple linear transformation", {
+
   data_example <- data.frame( ID = c(1,2,3,4,5),
                               puntaje = c(1,2,3,4,5))
   data_result <- data.frame(ID = c(1,2,3,4,5),
                             puntaje = c(1,2,3,4,5),
                             Transf_500 = c(445.4499, 451.7789,
                                            458.1079, 464.4370, 470.7660))
-  blue_ztransform <- function(x, data, meanvar = 500,sdvar = 100) {
-    standard <- scale(x, center = T, scale = T)
-    conversion <- data.frame((standard*sdvar)+meanvar)
-    data <- data.frame(data, Transf_500 = conversion[,1])
-    data}
-  expect_equal(blue_ztransform(data_example$puntaje,data_example),
-               data_result)
+   expect_equivalent(blue_ztransform(data_example$puntaje, data_example),data_result)
 })
 
 
